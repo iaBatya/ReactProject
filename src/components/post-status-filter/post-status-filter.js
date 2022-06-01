d9108cece024c217284551035ddb17abe04ca568
iaBatya <dmitryajax@gmail.com>
2022-06-01 20:27:29 +0300

    styles + foundation

---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostStatusFilter = () => {
+    return (
+        <div className="btn-group">
+            <button>Все</button>
+            <button>Добавлено в корзину</button>
+        </div>
+    )
+}
+
+export default PostStatusFilter;
